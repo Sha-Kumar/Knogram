e94ef600a7cfeae53d4c82e881af4bc5ddd64dde
Sha-Kumar <shashankneeded@gmail.com>
2020-09-16 20:55:58 +0530

    second comit

---
+++ lib/pages/login.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final emailField = TextField(
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Email",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    return Scaffold(
+        body: Center(
+      child: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(36.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // SizedBox(
+              //   height: 155.0,
+              //   child: Image.asset(
+              //     "assets/logo.png",
+              //     fit: BoxFit.contain,
+              //   ),
+              // ),
+
+              SizedBox(height: 50.0, width: 250.0, child: emailField,),
+              // emailField,
+              // SizedBox(height: 25.0, width: 100.0,),
+              // passwordField,
+              // SizedBox(
+              //   height: 35.0,width: 100,
+              // ),
+              // loginButon,
+              // SizedBox(
+              //   height: 15.0,
+              // ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
